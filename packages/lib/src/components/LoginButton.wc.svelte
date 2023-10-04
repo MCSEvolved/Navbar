1849e2f332306da7b840cfb5df0a6cefa598d678
IICarst <67209089+IICarst@users.noreply.github.com>
2023-10-04 11:20:20 +0200

    added redirect

--- packages/lib/src/components/LoginButton.wc.svelte
+++ packages/lib/src/components/LoginButton.wc.svelte
@@ -59,7 +59,7 @@
 {#if debug === "true"}
     <button on:click={signIn}> Log in </button>
 {:else}
-    <a href="/login">
+    <a href={`/login?redirect=${window.location.href}`}>
         <button> Log in </button>
     </a>
 {/if}
